b737581e852d0ea96fb72426e6a14f2c7729c85e
Jung WooSeok <56007468+w00shh@users.noreply.github.com>
2024-07-22 20:39:24 +0900

    fix: backup.sh 수정

--- backup.sh
+++ backup.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-TODAY=`date +%y-%m-%d`
-
-BACKUP_DIR="$TODAY"
-
-MONGO_URI=${MONGO_URI}
+TODAY=$(date +%Y-%m-%d)
+BACKUP_DIR="/backup/$TODAY"
 S3_BUCKET=${S3_BUCKET}
+MONGO_URI=${MONGO_URI}
 
+mongodump --uri="${MONGO_URI}" --out="${BACKUP_DIR}"
 
-mongodump --uri="$MONGO_URI" --out=BACKUP_DIR"
+aws s3 cp --recursive "${BACKUP_DIR}" "s3://${S3_BUCKET}/${TODAY}/"
 
-aws s3 cp --recursive "$BACKUP_DIR" "s3://$S3_BUCKET/$TODAY"
-
-rm -rf "$BACKUP_DIR"
+rm -rf "${BACKUP_DIR}"
